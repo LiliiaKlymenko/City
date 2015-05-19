ac8176fd189befdf2a896952b171c533224bc5c0
liliia_klymenko <liliia_klymenko@epam.com>
2015-05-19 18:16:30 +0300

    data provider changed

---
+++ src/test/java/Runner.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Liliia_Klymenko on 19-May-15.
+ */
+public class Runner {
+
+   /* public static void main(String[] args) {
+        TestNG testNG = new TestNG();
+        new Parser("testng.xml").parseToList().forEach(testNG::setCommandLineSuite);
+        testNG.run();
+    }*/
+}
